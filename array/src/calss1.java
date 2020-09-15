02ac6ff533bba958b23d6ba3fa4a32d96622445b
lirongrong <2270572070@qq.com>
2020-09-15 22:54:56 +0800

    数组补充

---
+++ array/src/calss1.java
@@ -0,0 +1,14 @@
+public class calss1 {
+    public static void main(String[] args) {
+        class person {
+            String name;
+            boolean isMale;
+            int age;
+
+
+        }
+    person person1 = new person();
+        person1.name = "tt";
+        System.out.println(person1.name);
+    }
+}
